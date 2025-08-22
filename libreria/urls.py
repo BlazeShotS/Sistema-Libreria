98cd300b5de105f91b8dae1cdd3efa951c1f81a1
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-08-21 23:59:12 -0500

    Rutas de libreria urls

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('', include('libreria.urls')), #accediendo a todo lo que tiene libreria.urls
 ]
