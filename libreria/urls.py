fca07b0ced9fad84fe4b19f7c84219e5d5880068
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-08-23 00:45:42 -0500

    html en index y editar

--- libreria/urls.py
+++ libreria/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'), #name='inicio' , ese inicio tengo que poner en mi href o ruta mejor dicho
     path('nosotros', views.nosotros, name= 'nosotros'),
-    path('libros', views.libros, name='libros')
+    path('libros', views.libros, name='libros'),
+    path('libros/crear', views.crear, name='crear')
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -6,5 +6,8 @@ def inicio(request):
     return render(request, 'paginas/inicio.html')
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+
 def libros(request):
     return render(request, 'libros/index.html')
+def crear(request):
+    return render(request, 'libros/crear.html')
