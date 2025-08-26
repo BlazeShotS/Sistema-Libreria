b41ad0da290a4d4c1179a3e507e947e76a5149a7
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-08-26 15:32:50 -0500

    Crud administrativo

--- libreria/admin.py
+++ libreria/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Libro #De models.py estoy importando el modelo Libro
 
 # Register your models here.
+admin.site.register(Libro) #Cuando corramos , el administrador tenga la manipulacion a ese libro 
