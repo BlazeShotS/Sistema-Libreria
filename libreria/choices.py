8b4a185e1467632228f8aa09daea260000a31d42
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-09-02 15:28:19 -0500

    wip: creacion del modelo y clave foranea

---
+++ libreria/choices.py
@@ -0,0 +1,7 @@
+sexos=( #Creamos una tupla, las tuplas son inmutables una vez creado no se pueden modificar sus elementos es como un array fijo y se declaran asi, pero si se pueden combinar los tipos de variables
+    ('F', 'Femenino'),
+    ('M', 'Masculino')
+)
+
+#Las listas es muy parecio a un array , pero no del todo porque un array en java tiene un tamaño fijo y es de un solo tipo de variable
+#encambio las listas en python , no tiene tamaño fijo se puede agregar o quitar y se pueden mezclar todo tipos de variable
--- libreria/models.py
+++ libreria/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 
+from .choices import sexos
+
+
 # Create your models here.
+
+class Autor(models.Model):
+    apellido_paterno = models.CharField(max_length=20, verbose_name='Apellido Paterno')
+    apellido_materno = models.CharField(max_length=20, verbose_name='Apellido Materno')
+    nombres = models.CharField(max_length=20, verbose_name='Nombres')
+    fecha_nacimiento=models.DateField(verbose_name='Fecha de nacimiento')
+    sexo = models.CharField(max_length=1, choices=sexos, default='F')
+
+    def nombre_completo(self):
+        return "{} {}, {}".format(self.apellido_paterno, self.apellido_materno, self.nombres)
+    
+    def __str__(self):
+        return self.nombre_completo()
+
+    class Meta:
+        verbose_name='Autor'
+        verbose_name_plural= 'Autores'
+        db_table='Autor'
+        ordering=['apellido_paterno','-apellido_materno'] #Forma descendente
+
 class Libro(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100, verbose_name='Titulo') #verbose sirve para mostrar al usuario como se llama ese campo
     imagen = models.ImageField(upload_to='imagenes/',verbose_name='Imagen' ,null=True) #imagenes/ es la carpeta que se creara a continuacion
     descripcion = models.TextField(null=True, verbose_name='Descripcion')
+    autor = models.ForeignKey(Autor,null=True,blank=True,on_delete=models.CASCADE) #ese CASCADE es si se borra un autor se borra todo los cursos relacionados con ese libro
 
     #Para que en la pagina admin de django, se visualize con estos nombres
     def __str__(self):
