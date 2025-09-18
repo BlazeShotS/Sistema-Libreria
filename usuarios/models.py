25321fb28c79b6099e78279e7df007e687696f34
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-09-17 20:40:47 -0500

    feat: modeo Usuario creado

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
+    
 # Create your models here.
+class Usuario(models.Model):
+    ROLES = ( #Tupla
+        ('ADMIN','Administrador'),
+        ('CLIENT','Cliente')
+    )
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.PositiveIntegerField()
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    rol = models.CharField(max_length=10, choices=ROLES, default='CLIENT')
+
+    def save(self, *args, **kwargs):
+        # Si la contraseña no está en hash, la encripta antes de guardar
+        if not self.password.startswith("pbkdf2_"):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+    def check_password(self, raw_password):
+        """Verifica si la contraseña ingresada coincide con el hash"""
+        return check_password(raw_password, self.password)
+
+    def nombre_completo(self):
+        return f"{self.nombre} {self.apellido} ({self.rol})"    
+
+    #Metodo especial en python que define como se representara un objeto como cadena __str__ DJANGO lo usa automaticamente en <select>, interfaz del admin, cuando imprimo el objeto autor y cuando lo pongo en una plantilla
+    def __str__(self):
+        return self.nombre_completo()
+    
+
+    class Meta:
+        verbose_name='Usuario'
+        verbose_name_plural= 'Usuarios'
+        db_table='usuario' #Nombre personalizado con el que se creara en la database
+        ordering=['nombre','apellido'] #Forma descendente , en el admin django
+    
+    
