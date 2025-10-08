134ab25e3a46e2cdafb4e5c3f5ea5522746e652f
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-10-08 18:41:35 -0500

    refactor: mejore la logica del codigo de registrar rol

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,6 +5,7 @@ class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = '__all__' #Con esto le digo que el formulario , tendra todo los campos de la tabla Usuario
+        exclude = ['rol']   #Oculta el campo 'rol' en el formulario
 
 class LoginForm(forms.Form): #En este caso no se crea desde la database o de un modelo , si no nosostros mismos personalizandolo en este caso para login
     email = forms.EmailField(label="Correo electronico") #type y name = email en el html
